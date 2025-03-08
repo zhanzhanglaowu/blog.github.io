bcfa5b0a0b791ebf7c4d192427c523b2f344e1bd
laowu <74804854+zhanzhanglaowu@users.noreply.github.com>
2025-03-08 11:21:53 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,29 @@
+﻿
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ca4dc03c400dea219d8f0a2facfcf124";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+window.onload = function() {
+	guanggao();
+}
+function guanggao() {
+	var xmlhttp;
+	if(window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById("guanggao").innerHTML = xmlhttp.responseText;
+			translate.execute();
+		}
+	}
+	xmlhttp.open("GET", "/guanggao.html", true);
+	xmlhttp.send();
+}
